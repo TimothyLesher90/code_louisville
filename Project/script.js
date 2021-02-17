b22d71e0832e7155469203400eb4bdf85cdac049
unknown <TimothyLesher90@gmail.com>
2021-02-16 22:03:36 -0500

    Removed fieldsets

--- Project/script.js
+++ Project/script.js
@@ -1,3 +1,7 @@
-const miles
-const kilometers
+const total //variable for total in the tip calculator
+const faren //variable for farenheight
+const cel //variavle for celceius
+const miles //variable for miles
+const kilometers //variable for kilometers
+
 
