54f8d862dc8f7358db441b385412a7e00be764ef
unknown <TimothyLesher90@gmail.com>
2021-02-21 10:39:06 -0500

    All Calculators working

--- Project/script.js
+++ Project/script.js
@@ -8,81 +8,20 @@ calc.addEventListener('click', function(){
 
     let tipamount = document.getElementById('tipamount').value = tipTotal/100*total;
     document.getElementById('grandTotal').value = parseFloat(total) + parseFloat(tipamount);
-
 })
     //End Tip Calculator code
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //Degree code
-function convertTemp() {
-        const degree = 32; //load 32 into degree for formula
-        const cel = 1.8; //load 1.8 into cel for formula
-        const fOn = (5/9); //load 5/9 into fOn for formula
-        var degreesF = document.querySelector("degreesF").value;
-        var degreesC = document.querySelector("degreesC").value;
+let Convert = document.getElementById('Convert');
 
-        degreesF = (degreesC * cel) + degree; //Convert Farenheight to Celcius F = (C * 1.8) +32
-        degreesC = (degreesF - degree) * fOn //convert Celcius to Farenheight C = (F - 32) * (5/9)
+Convert.addEventListener('click', function(){
+    let degreesF = document.getElementById('degreesF').value;
+    let degreesC =  document.getElementById('degreesC').value = (degreesF-32)/(1.8);
+})
+    //End of degree code
+    let convert = document.getElementById('convert');
 
-        //Validation to make sure there is numbers in labels
-
-        //if degreeF is blank run degree F and display The temperatur is degrees farenheight else run degreesC
-        if(degreeF.value === '') 
-        { 
-        return(degreesF);
-        }
-        else
-        {
-        return(degreesC);
-        }
-} //bracees for button
-    
-//button 
-convert.onclick = convertTemp();
-//End of degree code
-
-
-//Distance code
-function convertDistance() {
-    const number = 1.609;
-
-    //validation if labels are empty
-
-    //convert Miles to Kilometers
-
-    //Convert Kilometers
-
-}//bracees for button
-
-    //Output after conversion 
-
-//End of distance code
+    convert.addEventListener('click', function(){
+        let mi = document.getElementById('mi').value;
+        let km =  document.getElementById('km').value = mi*1.609;
+    })
